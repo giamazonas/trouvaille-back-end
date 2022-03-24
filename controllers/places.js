3a796d563fa58a0cf1d6be592c3f62a92259df74
maxmay94 <maxmay94@gmail.com>
2022-03-24 16:22:20 -0400

    stubbbiiinnngggg

--- controllers/places.js
+++ controllers/places.js
@@ -1,1 +1,24 @@
 import { Place } from '../models/place.js'
+
+function show(req, res) {
+
+}
+
+function create(req, res) {
+
+}
+
+function update(req, res) {
+
+}
+
+function deleteItinerary(req, res) {
+
+}
+
+export {
+  show,
+  create,
+  update,
+  deletePlace as delete
+}
