5120fc6f5d209269c4eea49702c8480ea9c475ee
Garam Jang <garamj88@gmail.com>
2022-03-24 17:01:40 -0400

    add models

--- models/profile.js
+++ models/profile.js
@@ -3,6 +3,9 @@ import mongoose from 'mongoose'
 const profileSchema = new mongoose.Schema({
   email: {type: String, required: true, lowercase: true, unique: true},
   name: String,
+  isAdmin: { type: Boolean, required: true, default: false },
+  itinerary: [{ type: Schema.Types.ObjectID, ref: 'Itinerary' }],
+  starred: [{ type: Schema.Types.ObjectID, ref: 'Place' }]
 },{
     timestamps: true,
 })
