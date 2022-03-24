92f9e979a90e37b26d6822216e7b688c3d2e1d0d
giamazonas <skibinskirenee@gmail.com>
2022-03-24 17:24:10 -0400

    add create city

--- controllers/cities.js
+++ controllers/cities.js
@@ -1,39 +1,37 @@
-import { City } from '../models/city.js'
+import { City } from "../models/city.js";
 
-function index (req, res) {
+function index(req, res) {
   City.find({})
-  .populate('owner')
-  .then(cities => {
-    res.json(cities)
-  })
-  .catch(err => {
-    res.json(err)
-  })
+    .populate("owner")
+    .then((cities) => {
+      res.json(cities);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 }
 
-
 function show(req, res) {
-
+  City.findById(req.params.id)
+    .then((city) => res.json(city))
+    .catch((err) => res.json(err));
 }
 
 function create(req, res) {
-
+  City.create(req.body)
+    .then((city) => {
+      city.populate("city").then((populateCity) => {
+        res.status(201).json(populateCity);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 }
 
-function update(req, res) {
+function update(req, res) {}
 
-}
+function deleteCity(req, res) {}
 
-function deleteCity(req, res) {
-
-}
-
-
-export {
-  index,
-  show,
-  create,
-  update,
-  deleteCity as delete,
-  
-}
+export { index, show, create, update, deleteCity as delete };
