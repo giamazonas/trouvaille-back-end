41e753395f9e00b7cdb8cb8c7a4d95ffa90659f8
giamazonas <skibinskirenee@gmail.com>
2022-03-25 14:07:03 -0400

    modify routes

--- routes/cities.js
+++ routes/cities.js
@@ -11,7 +11,7 @@ router.get('/', citiesCtrl.index)
 
 /* ----------- Private Routes ----------- */ 
 router.use(decodeUserFromToken)
-router.post('/', checkAuth, citiesCtrl.create)
+router.post('/add', checkAuth, citiesCtrl.create)
 router.put('/:id', checkAuth, citiesCtrl.update)
 router.delete('/:id', checkAuth, citiesCtrl.delete)
 
