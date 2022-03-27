d340a9c876b8614fde982c8d00add7f57d02c715
giamazonas <skibinskirenee@gmail.com>
2022-03-27 11:52:11 -0400

    add cloudinary

--- controllers/places.js
+++ controllers/places.js
@@ -1,10 +1,11 @@
 import { Place } from "../models/place.js";
+import { v2 as cloudinary } from "cloudinary";
 
 function index(req, res) {
   // console.log('SEARCH idx function PLACE', req.query)
   // const error = req.query.error;
   // if(req.query.id) {
-    // console.log('HITTING IF CONDITION')
+  // console.log('HITTING IF CONDITION')
   // }
   Place.find({})
     .then((places) => res.json(places))
@@ -22,26 +23,67 @@ function show(req, res) {
 
 function create(req, res) {
   console.log("========= req.body place controller: ========", req.body);
-
-  Place.create(req.body)
-    .then((place) => res.json(place))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  req.body.owner = req.user.profile;
+  if (req.body.photo === "undefined" || !req.files["photo"]) {
+    delete req.body["photo"];
+    Place.create(req.body)
+      .then((place) => res.json(place))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  } else {
+    const imageFile = req.files.photo.path;
+    cloudinary.uploader
+      .upload(imageFile, { tags: `${req.body.name}` })
+      .then((image) => {
+        req.body.photo = image.url;
+        Place.create(req.body)
+          .then((place) => {
+            place.populate("owner").then((populatedPlace) => {
+              res.status(201).json(populatedPlace);
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      });
+  }
 }
 
 function update(req, res) {
-  Place.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    .then((place) => {
-      place.populate("place").then((populatePlace) => {
-        res.status(201).json(populatePlace);
+  if (req.body.photo === "undefined" || !req.files["photo"]) {
+    delete req.body["photo"];
+    Place.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      .then((place) => {
+        place.populate("place").then((populatePlace) => {
+          res.status(201).json(populatePlace);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
       });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  } else {
+    const imageFile = req.files.photo.path;
+    cloudinary.uploader
+      .upload(imageFile, { tags: `${req.body.name}` })
+      .then((image) => {
+        console.log(image);
+        req.body.photo = image.url;
+        Place.findByIdAndUpdate(req.params.id, req.body, { new: true })
+          .then((place) => {
+            place.populate("owner").then((populatedPlace) => {
+              res.status(201).json(populatedPlace);
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      });
+  }
 }
 
 function deletePlace(req, res) {
