242f33c9250ad4dd8201c68f748bf70b2bd52d7a
giamazonas <skibinskirenee@gmail.com>
2022-03-27 09:27:50 -0400

    stash

--- controllers/cities.js
+++ controllers/cities.js
@@ -1,72 +1,70 @@
-import { City } from "../models/city.js";
+import { City } from '../models/city.js'
 
-function index(req, res) {
-  console.log("SEARCH idx function CITY", req.query);
-  const error = req.query.error;
-  if (req.query.City) {
-    console.log("HITTING IF CONDITION");
-    City.findById(req.query.city.city)
-      .populate("city")
-      .then((city) => {
-        if (city) {
-          res.render("cities/:id", {
-            city,
-            title: "City",
-            error,
-          });
-        } else {
-          throw new Error('Check spelling or try new search')  
-        }
-      });
-  } else {
-    City.find({}).then((cities) => res.json(cities));
-  } 
+function index (req, res) {
+  City.find({})
+  .populate('owner')
+  .then(cities => {
+    res.json(cities)
+  })
+  .catch(err => {
+    res.json(err)
+  })
 }
 
 function show(req, res) {
   City.findById(req.params.id)
-    .then((city) => res.json(city))
-    .catch((err) => {
-      console.log(err);
-      res.json(err);
-    });
+  .then(city => res.json(city))
+  .catch(err => {
+    console.log(err)
+    res.json(err)
+  })
 }
 
 function create(req, res) {
-  console.log("hello");
+  console.log('hello')
   City.create(req.body)
-    .then((city) => res.json(city))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  .then(city => res.json(city))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 }
 
 function update(req, res) {
-  City.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    .then((city) => res.json(city))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  City.findByIdAndUpdate(req.params.id, req.body, {new:true})
+  .then(city => res.json(city))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 }
 
 function edit(req, res) {
-  City.findById(req.params.id, req.body)
-    .then((city) => res.json(city))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  console.log('EDIT function')
+  City.findById(req.params.id, req.body,)
+  .then(city => res.json(city))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 }
+
 
 function deleteCity(req, res) {
   City.findByIdAndDelete(req.params.id)
-    .then((city) => res.json(city))
-    .catch((err) => {
-      console.log(err);
-      res.json(err);
-    });
+  .then(city => res.json(city))
+  .catch(err => {
+    console.log(err)
+    res.json(err)
+  })
 }
 
-export { index, show, create, update, edit, deleteCity as delete };
+export {
+  index,
+  show,
+  create,
+  update,
+  edit,
+  deleteCity as delete,
+  
+}
