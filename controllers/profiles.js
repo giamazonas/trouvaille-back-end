16f133b53934e01207996135ddff222fa7454d17
maxmay94 <maxmay94@gmail.com>
2022-03-31 17:53:02 -0400

    itineraries show up

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,4 +1,5 @@
-import { Profile } from "../models/profile.js";
+import { Itinerary } from "../models/itinerary.js"
+import { Profile } from "../models/profile.js"
 
 function index(req, res) {
   Profile.find({})
@@ -17,7 +18,28 @@ function addItinerary(req, res) {
   })
 }
 
+function showItineraries(req, res) {
+  Profile.findById(req.params.id)
+  .then(profile => profile.populate('itineraries'))
+  .then((data) => res.json(data))
+  .catch((err) => {
+    console.log(err)
+    res.status(500).json(err)
+  })
+}
+  // .then(data => {
+  //   console.log(data)
+    // data.itineraries.forEach((itinerary, i) => {
+      // console.log(itinerary)
+      // itinerary.timePlace.forEach(place => {
+      //   console.log(place.places)
+      //   console.log(place.time)
+      // })
+    // })
+  // })
+
 export { 
   index,
-  addItinerary
+  addItinerary,
+  showItineraries
 }
--- routes/itineraries.js
+++ routes/itineraries.js
@@ -5,13 +5,14 @@ import { decodeUserFromToken, checkAuth } from "../middleware/auth.js";
 const router = Router();
 
 /* ----------- Public Routes ----------- */
-router.get("/", itinerariesCtrl.index);
-router.get("/:id", itinerariesCtrl.show);
+router.get("/", itinerariesCtrl.index)
 
 /* ----------- Private Routes ----------- */
 router.use(decodeUserFromToken);
-router.post("/add", checkAuth, itinerariesCtrl.create);
-router.put("/:id", checkAuth, itinerariesCtrl.update);
-router.delete("/:id", checkAuth, itinerariesCtrl.delete);
 
-export { router };
+router.get("/:id", checkAuth, itinerariesCtrl.show)
+router.post("/add", checkAuth, itinerariesCtrl.create)
+router.put("/:id", checkAuth, itinerariesCtrl.update)
+router.delete("/:id", checkAuth, itinerariesCtrl.delete)
+
+export { router }
