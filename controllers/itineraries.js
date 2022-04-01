df7c18d29fa7d38ff2133672131be32de1b34e51
giamazonas <skibinskirenee@gmail.com>
2022-04-01 00:43:59 -0400

    stash

--- controllers/itineraries.js
+++ controllers/itineraries.js
@@ -1,37 +1,35 @@
-import { Itinerary } from "../models/itinerary.js"
+import { Itinerary } from "../models/itinerary.js";
 
-function index(req, res) {
-
-}
+function index(req, res) {}
 
 function show(req, res) {
-  req.body.owner = req.user.profile
-  console.log(req.body)
-  Itinerary.find({})
+  req.body.owner = req.user.profile;
+  console.log(req.body);
+  Itinerary.find({});
 }
 
 async function create(req, res) {
-  if(Object.keys(req.body[0].length === 0)) delete req.body[0]
-  req.body.owner = req.user.profile
+  if (Object.keys(req.body[0].length === 0)) delete req.body[0];
+  req.body.owner = req.user.profile;
   // console.log(req.body)
   const itinerary = await Itinerary.create({
-    name: req.body['24'].name,
-    owner: req.body.owner
-  })
-  
-  const keys = Object.keys(req.body)
+    name: req.body["24"].name,
+    owner: req.body.owner,
+  });
+
+  const keys = Object.keys(req.body);
   Object.values(req.body).forEach((item, i) => {
-    if(keys[i] !== '24' && keys[i] !== 'owner' ) {
+    if (keys[i] !== "24" && keys[i] !== "owner") {
       // console.log(keys[i], item)
-      const time = keys[i]
-      let obj = {}
-      obj['time'] = time
-      obj['places'] = item
-      itinerary.timePlace.push(obj)
+      const time = keys[i];
+      let obj = {};
+      obj["time"] = time;
+      obj["places"] = item;
+      itinerary.timePlace.push(obj);
     }
-  })
-  itinerary.save()
-  res.status(201).json(itinerary)
+  });
+  itinerary.save();
+  res.status(201).json(itinerary);
 }
 
 function update(req, res) {}
@@ -39,13 +37,7 @@ function update(req, res) {}
 function deleteItinerary(req, res) {
   Itinerary.findByIdAndDelete(req.params.id)
     .then((itinerary) => res.json(itinerary))
-    .catch((err) => res.json(err))
+    .catch((err) => res.json(err));
 }
 
-export { 
-  index, 
-  show, 
-  create, 
-  update, 
-  deleteItinerary as delete 
-}
+export { index, show, create, update, deleteItinerary as delete };
